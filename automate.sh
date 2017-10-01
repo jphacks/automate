67291b3a3117a7a80cc0da8e34a9aaf982beb562
takayukioda <takayuki@odashi.me>
2017-10-02 02:13:34 +0900

    Add automate script

---
+++ automate.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+readonly ORGANIZATION="jphacks"
+
+prepare() {
+	local prefix=$1
+	local limit=$2
+	local offset=$3
+
+	node prefix-loop.js $prefix $limit $offset | xargs -n1 -I@ \
+		node mkrepo.js @ $ORGANIZATION
+	node prefix-loop.js $prefix $limit $offset | xargs -n1 -I@ \
+		bash push-init.sh ~/github.com/jphacks/sample @ $ORGANIZATION
+}
+: "2017 札幌" || {
+	prepare SP_17 15 1
+}
+
+: "2017 仙台" || {
+	prepare SD_17 15 1
+}
+
+: "2017 名古屋" || {
+	prepare NG_17 15 1
+}
+
+: "2017 神戸" || {
+	prepare KB_17 15 1
+}
+
+: "2017 福岡" || {
+	prepare FK_17 15 1
+}
+
+: "2017 東京" || {
+	prepare TK_17 30 1
+}
